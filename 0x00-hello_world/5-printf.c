365ff6eccbdb0b8b5af8016582ea5e994dace7b0
Ahmed Wael <xahmedwaheed@gmail.com>
2023-08-10 13:44:33 -0700

    aaaaah

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - printf
+ *
+ *Description: prints with printf
+ *Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
