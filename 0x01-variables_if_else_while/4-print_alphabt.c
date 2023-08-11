22a6c4be7a0377e1b029ce67f372d93336ffb3b4
Ahmed Wael <xahmedwaheed@gmail.com>
2023-08-11 13:34:26 -0700

    even more alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets
+ *
+ * Description: prints the whole alphabet with putchar
+ * Return:0
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		if (a != 'e' && a != 'q')
+			putchar(a++);
+	}
+	putchar('\n');
+	return (0);
+}
