db1f1a38220b902a9dc9cd6a1b41fb88c076e91f
Ahmed Wael <xahmedwaheed@gmail.com>
2023-08-11 15:36:35 -0700

    done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets
+ *
+ * Description: prints the whole alphabet with putchar
+ * Return:0
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		putchar('0' + a);
+		if (a != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		a++;
+	}
+	return (0);
+}
