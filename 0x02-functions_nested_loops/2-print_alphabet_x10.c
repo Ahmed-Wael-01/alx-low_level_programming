2955241606417e9211138d71c667e3526f8a0235
unknown <Xahmedwaheed@gmail.com>
2023-08-16 00:07:59 +0300

    fk you betty

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - print alpha
+*
+* Description: it does smthing
+* Return: 0
+*/
+void print_alphabet_x10(void)
+{
+	int i;
+	for (i = 0 ; i < 10 ; i++)
+	{
+		char a = 'a';
+
+		while (a <= 'z')
+		{
+			_putchar(a++);
+		}
+		_putchar('\n');
+	}
+}
