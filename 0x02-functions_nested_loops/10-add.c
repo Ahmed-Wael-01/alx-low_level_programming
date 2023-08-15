41f9a31834eb1d5a8096ac6dcc083279d2dd49f4
unknown <Xahmedwaheed@gmail.com>
2023-08-16 01:58:27 +0300

    fk you betty

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*  add - print alpha
+* @n: the number to compare
+* @a: another number!!!!!
+*
+* Description: it does smthing
+* Return: 0
+*/
+int add(int n, int a)
+{
+	return (n + a);
+}
