e5e74383d69bc58789acffd49237898ac2002e90
unknown <Xahmedwaheed@gmail.com>
2023-08-16 01:11:54 +0300

    fk you betty

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,8 +9,10 @@
 */
 int print_last_digit(int n)
 {
-	int remainder = +(n % 10);
+	int remainder = (n % 10);
 
+	if (remainder < 0)
+		remainder = remainder * -1;
 	_putchar(remainder + '0');
 	return (remainder);
 }
