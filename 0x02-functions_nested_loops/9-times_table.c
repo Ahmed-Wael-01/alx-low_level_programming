33403bc1f9116c033a7fdabbd214660ee8c2b13f
unknown <Xahmedwaheed@gmail.com>
2023-08-16 01:53:21 +0300

    fk you betty

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,20 +17,28 @@ void times_table(void)
 		{
 			int total = j * i;
 
-			if (total < 10)
+			if (j != 0)
 			{
-				_putchar('0' + total);
+				if (total < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + total);
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + (total / 10));
+					_putchar('0' + (total % 10));
+				}
 			}
 			else
 			{
-				_putchar('0' + (total / 10));
-				_putchar('0' + (total % 10));
+				_putchar('0');
 			}
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
 		}
-		_putchar('$');
 		_putchar('\n');
 	}
 }
