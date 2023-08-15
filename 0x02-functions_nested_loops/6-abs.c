29d4dd3297e381768ee88f01eae3e483a8726468
unknown <Xahmedwaheed@gmail.com>
2023-08-16 00:48:52 +0300

    fk you betty

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*  _abs - print alpha
+* @n: the number to compare
+*
+* Description: it does smthing
+* Return: 0
+*/
+int _abs(int n)
+{
+	int remain;
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (- n);
+	}
+}
