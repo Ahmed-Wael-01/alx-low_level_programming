d28b022f8e45dbb92039f3ae660ab08547248b2e
unknown <Xahmedwaheed@gmail.com>
2023-08-16 02:41:31 +0300

    fk you betty

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,38 +11,34 @@ void print_times_table(int n)
 {
 	int i, j;
 
-    if (n >= 0 && n <= 15)
-    {
-	for (i = 0 ; i < n ; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0 ; j < n ; j++)
+	for (i = 0 ; i <= n ; i++)
+	{
+		for (j = 0 ; j <= n ; j++)
 		{
 			int total = j * i;
 
 			if (j != 0)
 			{
-                if (total >= 100)
-                {
-                    _putchar(',');
-					_putchar(' ');
-                    _putchar('0' + (total / 100));
+				_putchar(',');
+				_putchar(' ');
+				if (total >= 100)
+				{
+					_putchar('0' + (total / 100));
 					_putchar('0' + (total % 100) / 10);
 					_putchar('0' + (total % 10));
-                }
+				}
 				else if (total >= 10)
 				{
-					_putchar(',');
 					_putchar(' ');
-                    _putchar(' ');
 					_putchar('0' + (total / 10));
 					_putchar('0' + (total % 10));
 				}
 				else
 				{
-					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-                    _putchar(' ');
 					_putchar('0' + (total % 10));
 				}
 			}
@@ -51,5 +47,5 @@ void print_times_table(int n)
 		}
 		_putchar('\n');
 	}
-    }
+	}
 }
