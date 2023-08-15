6a8055761fbe3ed9b6a71338420f76b4f9e32663
unknown <Xahmedwaheed@gmail.com>
2023-08-16 00:55:28 +0300

    fk you betty

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* print_last_digit - print alpha
+* @n: the number to compare
+*
+* Description: it does smthing
+* Return: 0
+*/
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
