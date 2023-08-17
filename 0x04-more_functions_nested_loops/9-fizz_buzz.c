d355008ca7048dadc8ec1e95ffe80d469d868a5f
unknown <Xahmedwaheed@gmail.com>
2023-08-18 01:30:50 +0300

    tenth

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - it does something
+*
+* Description: function that checks for uppercase character
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100 ; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if (i % 3 != 0 && i % 5 != 0)
+			printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
