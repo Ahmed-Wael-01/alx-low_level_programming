17df1bbbcd75198ab14e4a2b85bbb341aca7f56b
unknown <Xahmedwaheed@gmail.com>
2023-08-18 00:48:38 +0300

    seventh

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_line - it does something
+* @n: a variable
+*
+* Description: function that checks for uppercase character
+* Return: 0
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+}
