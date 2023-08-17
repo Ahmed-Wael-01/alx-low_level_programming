bdf3da216eff8567e155bf4c34287b31e3e04ed6
unknown <Xahmedwaheed@gmail.com>
2023-08-18 00:58:14 +0300

    eighth

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,11 +11,15 @@
 void print_diagonal(int n)
 {
 	int i;
+	int j;
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar('0');
-	}
+		for (j = 0; j <= i; j++)
+		{
+			_putchar('0');
+		}
 	_putchar('\\');
 	_putchar('\n');
+	}
 }
