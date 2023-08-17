dd531e009715106cfd7556473e81c58ee8f99751
unknown <Xahmedwaheed@gmail.com>
2023-08-18 00:19:50 +0300

    fourth

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_numbers - it does something
+*
+* Description: function that checks for uppercase character
+* Return: 0
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
