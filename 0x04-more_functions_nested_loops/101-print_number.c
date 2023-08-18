bf197c3a57097c4f949bcd8c7a4d42ac577ebd74
unknown <Xahmedwaheed@gmail.com>
2023-08-18 08:10:39 +0300

    fixes

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,21 +10,21 @@
 
 void print_number(int n)
 {
-	unsigned int i = 1;
-	unsigned int num = n;
+    unsigned int i = 1;
+    unsigned int num = n;
 
-	if (n < 0)
-	{
-		num = -n;
-		_putchar('-');
-	}
-	while (num / i > 9 )
-	{
-		i *= 10;
-	}
-	while (i > 0)
-	{
-		_putchar('0' + ((num / i) % 10));
-		i /= 10;
-	}
+    if (n < 0)
+    {
+        num = -n;
+        _putchar('-');
+    }
+    while (num / i > 9)
+    {
+        i *= 10;
+    }
+    while (i > 0)
+    {
+        _putchar('0' + ((num / i) % 10));
+        i /= 10;
+    }
 }
