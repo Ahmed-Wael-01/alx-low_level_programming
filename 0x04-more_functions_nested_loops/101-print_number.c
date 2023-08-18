5898422c4422022cd8561f96e2e83076b8043ba6
unknown <Xahmedwaheed@gmail.com>
2023-08-18 05:36:03 +0300

    special_two

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,23 +11,23 @@
 void print_number(int n)
 {
 	int i = 10;
-    unsigned int num = n;
+	unsigned int num = n;
 
-    if (n < 0)
-    {
-        num = -n;
-        _putchar('-');
-    }
-    while (num / i > 0 )
-    {
-        i *= 10;
-    }
+	if (n < 0)
+	{
+		num = -n;
+		_putchar('-');
+	}
+	while (num / i > 0 )
+	{
+		i *= 10;
+	}
 
-    i /= 10;
+	i /= 10;
 
-    while (i > 0)
-    {
-        _putchar('0' + ((num / i) % 10));
-        i /= 10;
-    }
+	while (i > 0)
+	{
+		_putchar('0' + ((num / i) % 10));
+		i /= 10;
+	}
 }
