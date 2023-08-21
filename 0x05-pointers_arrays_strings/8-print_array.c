02f906be31dc2fd1250368953059d9e66ae629f2
unknown <Xahmedwaheed@gmail.com>
2023-08-22 00:55:40 +0300

    eight

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_array - does something
+ * @a: a variable
+ * @n: a variable
+ *
+ * Description: a function
+*/
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (i + 1 != n)
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+		i++;
+	}
+}
