c1d5a345b627492fb5007875c3c3ef89becd0d37
unknown <Xahmedwaheed@gmail.com>
2023-08-22 01:37:13 +0300

    nine

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,17 +6,18 @@
  * @src: a variable
  *
  * Description: a function
- * Retuen: dest
+ * Return: dest
 */
 
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (src[i - 1] != '\0')
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
+	dest[i] = src[i];
 	return (dest);
 }
