09dfea4b4280c19a935eace4db258ed3e19b95aa
unknown <Xahmedwaheed@gmail.com>
2023-08-21 22:56:58 +0300

    one

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - does something
+ * @a: a variable
+ * @b: a variable
+ *
+ * Description: a function
+*/
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
