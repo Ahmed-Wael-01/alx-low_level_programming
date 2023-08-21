9f909bcf918e71d7d7cb836d527b16193a4c6884
unknown <Xahmedwaheed@gmail.com>
2023-08-22 01:13:22 +0300

    nine

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_array - does something
+ * @dest: a variable
+ * @src: a variable
+ *
+ * Description: a function
+ * Retuen: dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	*dest = *src;
+	return (*dest);
+}
