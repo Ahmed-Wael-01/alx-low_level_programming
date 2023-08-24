c2d444ca1323ea9b577cdfe91362f09bfe117839
unknown <Xahmedwaheed@gmail.com>
2023-08-25 02:19:25 +0300

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rot13 - does something
+ * @str: a variable
+ *
+ * Description: a function
+ * Return: str
+*/
+
+char *rot13(char *str)
+{
+	char values[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char newVal[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == values[j])
+				str[i] = newVal[j];
+		}
+	}
+	return (str);
+}
