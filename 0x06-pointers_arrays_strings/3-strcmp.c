a42641cdd665c8ba2b29de951e75509fb4a466a1
unknown <Xahmedwaheed@gmail.com>
2023-08-25 00:39:37 +0300

    three

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,16 +11,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-    int i;
+	int i;
 
-    for (i = 0; s1[i] != '\0'; i++)
-    {
-        if (s1[i] == s2[i])
-            continue;
-        else if (s1[i] > s2[i])
-            return (1);
-        else
-            return (-1);
-    }
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] == s2[i])
+			continue;
+		else if (s1[i] > s2[i])
+			return (s1[i] - s2[i]);
+		else
+			return (s1[i] - s2[i]);
+	}
 	return (0);
 }
