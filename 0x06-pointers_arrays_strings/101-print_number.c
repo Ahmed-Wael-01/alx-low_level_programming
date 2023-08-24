7e18a682277c1561f090bdff3c20b418ea3c4522
unknown <Xahmedwaheed@gmail.com>
2023-08-25 02:28:24 +0300

    printNums

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* print_number - it does something
+* @n: a variable
+*
+* Description: function that checks for uppercase character
+* Return: 0
+*/
+
+void print_number(int n)
+{
+	unsigned int i = 1;
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		num = -n;
+
+		_putchar('-');
+	}
+
+	while (num / i > 9)
+	{
+		i *= 10;
+	}
+
+	while (i > 0)
+	{
+		_putchar('0' + ((num / i) % 10));
+		i /= 10;
+	}
+}
