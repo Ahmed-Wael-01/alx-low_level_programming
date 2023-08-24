6f3aca19d6a444a941f9c181923ddd955142c64e
unknown <Xahmedwaheed@gmail.com>
2023-08-25 01:09:08 +0300

    five

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,17 +2,17 @@
 
 /**
  * string_toupper - does something
- *@str: a variable
+ * @str: a variable
  *
  * Description: a function
- * Return: dest
+ * Return: str
 */
 
 char *string_toupper(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0 ; str[i] != '\0' ; i++)
 	{
 		if (str[i] >= 97 && str[i] <= 122)
 			str[i] -= 32; 
