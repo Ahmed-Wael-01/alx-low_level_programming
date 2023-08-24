c78c5b27cedcad7c0006faa0c0b8243dfff30d25
unknown <Xahmedwaheed@gmail.com>
2023-08-24 23:32:20 +0300

    second

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -20,18 +20,13 @@ char *_strncpy(char *dest, char *src, int n)
 		{
 		dest[i] = src[i];
 		i++;
-			if (src[i] == '\0')
-			{
-				dest[i] = '\0';
-				return (dest);
-			}
 		}
 		else
 			break;
 	}
-	i = 0;
-	while (dest[i] != '\0')
+	while (i < n)
 	{
+		dest[i] = '\0';
 		i++;
 	}
 	dest[i] = '\0';
