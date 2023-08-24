0344c13558708346507a32114f613f9aab21f202
unknown <Xahmedwaheed@gmail.com>
2023-08-25 01:33:54 +0300

    six

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,14 +15,16 @@ char *cap_string(char *str)
 	for (i = 0 ; str[i] != '\0' ; i++)
 	{
 		if (i == 0)
+		{
 			if (str[i] >= 97 && str[i] <= 122)
 				str[i] -= 32;
-		else if (str[i - 1] == " " || str[i - 1] == "," 
-		|| str[i - 1] == ";" || str[i - 1] == "."
-		|| str[i - 1] == "!" || str[i - 1] == "?"
-		|| str[i - 1] == "\"" || str[i - 1] == "("
-		|| str[i - 1] == ")" || str[i - 1] == "{"
-		|| str[i - 1] == "}" || str[i - 1] == "\n")
+		}
+		else if (str[i - 1] == ' ' || str[i - 1] == ',' 
+		|| str[i - 1] == ';' || str[i - 1] == '.'
+		|| str[i - 1] == '!' || str[i - 1] == '?'
+		|| str[i - 1] == '\"' || str[i - 1] == '('
+		|| str[i - 1] == ')' || str[i - 1] == '{'
+		|| str[i - 1] == '}' || str[i - 1] == '\n')
 		{
 			if (str[i] >= 97 && str[i] <= 122)
 				str[i] -= 32;
