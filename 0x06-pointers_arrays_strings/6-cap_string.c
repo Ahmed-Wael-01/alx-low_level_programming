961d8d11dccc3dc20a60e2ac74ef7a309f041e9a
unknown <Xahmedwaheed@gmail.com>
2023-08-25 01:25:41 +0300

    six

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * cap_string - does something
+ * @str: a variable
+ *
+ * Description: a function
+ * Return: str
+*/
+
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (str[i - 1] == " " || str[i - 1] == "," 
+		|| str[i - 1] == ";" || str[i - 1] == "."
+		|| str[i - 1] == "!" || str[i - 1] == "?"
+		|| str[i - 1] == "\"" || str[i - 1] == "("
+		|| str[i - 1] == ")" || str[i - 1] == "{"
+		|| str[i - 1] == "}" || str[i - 1] == "\n")
+		{
+			if (str[i] >= 97 && str[i] <= 122)
+				str[i] -= 32;
+		}
+	}
+	return (str);
+}
