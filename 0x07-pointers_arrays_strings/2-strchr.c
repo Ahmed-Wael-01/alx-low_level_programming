30656634f025e537797a93788e5604d385e1e2ae
unknown <Xahmedwaheed@gmail.com>
2023-08-28 16:12:29 +0300

    second com

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,11 +13,11 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (*(s + i) >= '\0')
+	while (*(s + i) != '\0')
 	{
 		if (*(s + i) == c)
 			return (s + i);
 		i++;
 	}
-	return ('\0');
+	return (0);
 }
