49b1f17bcf1e7f6c99fc8bc950cba916ba70b312
unknown <Xahmedwaheed@gmail.com>
2023-08-31 01:49:58 +0300

    here we go again

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -14,8 +14,5 @@ int factorial(int n)
 		return (-1);
 	else if (n == 0)
 		return (1);
-	else
-	{
-		return (factorial(n - 1) * n);
-	}
+	return (factorial(n - 1) * n);
 }
---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - does something
+ * @x: a variable
+ * @y: a variable
+ *
+ * Description: a function
+ * Return: 0
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+		return (_pow_recursion(x, y - 1) * x);
+	else
+		return (1);
+}
