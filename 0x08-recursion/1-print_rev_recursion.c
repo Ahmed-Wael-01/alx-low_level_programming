5aedd1a125412f970449d86babb66efc62f0ca3a
unknown <Xahmedwaheed@gmail.com>
2023-08-31 00:55:16 +0300

    here we go again

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - does something
+ * @s: a variable
+ *
+ * Description: a function
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+        s++;
+        _print_rev_recursion(s);
+	}
+    _putchar(*--s);
+}
