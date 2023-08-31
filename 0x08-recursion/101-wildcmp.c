4946b38a23bbb2b8fa99f9b60acff1ad4ccbbd33
unknown <Xahmedwaheed@gmail.com>
2023-08-31 05:12:35 +0300

    here we go again

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * wildcmp - does something
+ * @s: a variable
+ * @s: a variable
+ * Description: a function
+ * Return: 0
+*/
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1));
+	else if (*s1 == *s2 && *s1 == '\0')
+		return (1);
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	else if (*s1 != *s2 && *(s2 - 1) == '*' && *s2 == '\0')
+		return (1);
+	else if (*s1 != *s2 && *(s2 - 1) == '*' && *s1 != '\0')
+		return (wildcmp(s1 + 1, s2));
+	else
+		return (0);
+
+}
