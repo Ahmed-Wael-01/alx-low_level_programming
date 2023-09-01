c2856acd987535fe9732a48010ee9dc131a72888
unknown <Xahmedwaheed@gmail.com>
2023-09-02 02:57:02 +0300

    third

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,13 +11,13 @@
  */
 int main(int argc, char *argv[])
 {
-    int i = 0;
-    (void) argc;
+	int i = 0;
+	(void) argc;
 
-    while (argv[i] != 0)
-    {
-        printf("%s\n", argv[i]);
-        i++;
-    }
+	while (argv[i] != 0)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
 	return (0);
 }
