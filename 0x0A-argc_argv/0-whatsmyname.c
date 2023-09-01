bb153f8122d387c5f8c48b49697d463ecd9ece08
unknown <Xahmedwaheed@gmail.com>
2023-09-02 02:05:42 +0300

    first

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * @argc: number
+ * @argv: parameter
+ *
+ * Description: does somehing
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	_putchar(argv[0]);
+	_putchar('\n');
+
+	return (0);
+}
