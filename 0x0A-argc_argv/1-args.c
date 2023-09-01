f9bb9197d21be36ee45b16b3f35054f6e5359cf7
unknown <Xahmedwaheed@gmail.com>
2023-09-02 02:41:18 +0300

    second

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ * @argc: number
+ * @argv: parameter
+ *
+ * Description: does somehing
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
