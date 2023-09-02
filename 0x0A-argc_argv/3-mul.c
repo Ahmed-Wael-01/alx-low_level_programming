b35bac6dfc19568152cccd949bfc45ef68d6db65
unknown <Xahmedwaheed@gmail.com>
2023-09-02 03:03:57 +0300

    third

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ * @argc: number
+ * @argv: parameter
+ *
+ * Description: does somehing
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
