b6d050fe127e60ca783390897803aca0ffc04224
unknown <Xahmedwaheed@gmail.com>
2023-09-04 12:42:48 +0300

    third

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,7 +26,7 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		*(square + i) = malloc(sizeof(int) * width);
-		if (!square)
+		if (!*(square + i))
 		{
 			free(square);
 			return (NULL);
