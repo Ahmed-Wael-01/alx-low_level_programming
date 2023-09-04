17609889427c4a66971f300bc1a5ac41dbf45a5b
unknown <Xahmedwaheed@gmail.com>
2023-09-04 10:22:50 +0300

    in the name of the lord

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *ar = malloc(size * sizeof(c));
 
-	if (!size || !*ar)
+	if (!size || !ar)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
