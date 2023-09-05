af856c7e872f773dd6647ca353e34d945007f6d7
unknown <Xahmedwaheed@gmail.com>
2023-09-05 13:08:18 +0300

    billion

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -41,7 +41,7 @@ char **strtow(char *str)
 {
 	int i, j, counter, bp = 0;
 	int words = wordcount(str);
-	char **ptr = malloc(sizeof(char *) * words + 1);
+	char **ptr = malloc(sizeof(char *) * words);
 
 	if (str == NULL || *str == '\0' || !ptr || !words)
 		return (NULL);
@@ -51,13 +51,13 @@ char **strtow(char *str)
 			bp++;
 		for (counter = 0; *(str + bp + counter) != ' '; counter++)
 			;
-		*(ptr + i) = malloc(sizeof(char) * counter + 1);
+		*(ptr + i) = malloc(sizeof(char) * counter);
 		if (!*(ptr + i))
 			return (NULL);
 		for (j = 0; *(str + bp) != ' ' && *(str + bp) != '\0'; j++, bp++)
 			*(*(ptr + i) + j) = *(str + bp);
 		*(*(ptr + i) + j) = '\0';
 	}
-	*(ptr + i) = NULL;
+	/**(ptr + i) = NULL;*/
 	return (ptr);
 }
