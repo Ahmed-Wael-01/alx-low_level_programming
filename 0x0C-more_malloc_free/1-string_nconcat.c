e08ac9dfdd0aedcea7a207856608ad5536bc24fe
unknown <Xahmedwaheed@gmail.com>
2023-09-08 00:53:55 +0300

    first

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= len2)
 		n = len2;
 	ptr = malloc(sizeof(char) * (len1 + n) + 1);
-	if (ptr = NULL)
+	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < len1; i++)
 		*(ptr + i) = *(s1 + i);
