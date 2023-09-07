8e2a3d592715f097348075d2c2889d08394f7938
unknown <Xahmedwaheed@gmail.com>
2023-09-08 00:31:12 +0300

    alpha

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - does something
+ * @b: a variable
+ *
+ * Description: a function
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+    if (ptr == NULL)
+        exit(98);
+    else
+        return (ptr);
+}
