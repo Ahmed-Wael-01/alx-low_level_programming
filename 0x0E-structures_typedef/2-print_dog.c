e0c24a6de568e88d6b0adee0ff78e431caa89252
Ahmed Wael <xahmedwaheed@gmail.com>
2023-09-11 07:03:15 -0700

    seond

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - initialize dogs
+ * @d: a var
+ *
+ * Description: it sets the value of functions inside the structure
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != 0)
+	{
+		if (d->age != 0)
+			printf("Name: %s\nAge: %f\nOwner: %s", (d->name ? d->name : "(nil)"),
+				d->age, (d->name ? d->name : "(nil)"));
+		else
+			printf("Name: %s\nAge: (nil)\nOwner: %s", (d->name ? d->name : "(nil)"),
+					(d->name ? d->name : "(nil)"));
+	}
+}
