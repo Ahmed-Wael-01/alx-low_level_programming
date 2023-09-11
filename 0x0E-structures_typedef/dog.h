34049b79c7896944ca159bd436e008541f89cc3d
Ahmed Wael <xahmedwaheed@gmail.com>
2023-09-11 06:19:08 -0700

    zero

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef STRC
+#define STRC
+
+/**
+ * struct dog - some structure
+ * @name: a var
+ * @age: a var
+ * @owner: a var
+ * Description: a structure for dogs
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
