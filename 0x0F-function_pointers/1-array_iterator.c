6a57f48968af4731be08a0008135e790a6cecc28
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2023-09-13 16:15:38 +0300

    first

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_iterator - it is a function
+ * @array: a parameter
+ * @size: a parameter
+ * @action: a parameter
+ *
+ * Description: it does something
+ * Return: 0
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #define PTR
 
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
 
