6bc0a5e01b2d328d2823f7d4ce32853429ba426c
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2023-09-14 04:14:08 +0300

    calculator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -22,8 +22,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*' &&
-	argv[2][0] != '/' && argv[2][0] != '%')
+	if (argv[2][1] != '\0' || get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
