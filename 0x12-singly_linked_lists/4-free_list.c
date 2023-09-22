a4a6b2c9716777f677d4ce57873162233f7343f2
Ahmed Wael <xahmedwaheed@gmail.com>
2023-09-22 15:30:50 -0700

    four

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - returns number of elements
+ *
+ * Description: it does something
+ */
+
+void free_list(list_t *head)
+{
+	list_t *new_head;
+
+	while (head != NULL)
+	{
+		new_head = head->next;
+		free(head->str);
+		free(head);
+		head = new_head;
+	}
+}
