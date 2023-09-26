039bfbebeff24344357092e78f5ff5951f908db8
Ahmed Wael <xahmedwaheed@gmail.com>
2023-09-26 13:50:43 -0700

    nine

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	while (curr != NULL)
 	{
-		if (i == idx)
+		if (i == idx - 1)
 		{
 			new->next = curr->next;
 			curr->next = new;
