8ed4e4b236d9a73436b2035eb35cd1b8c10f76da
Ahmed Wael <xahmedwaheed@gmail.com>
2023-09-26 06:36:57 -0700

    five

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees the dogs
+ * @head: head of the list
+ *
+ * Description: it does something
+ * Return: 0
+ */
+
+void free_listint2(listint_t **head)
+{
+        listint_t *temp;
+
+        if (*head == NULL)
+                return;
+        while ((*head)->next != NULL)
+        {
+                temp = (*head)->next;
+                free(*head);
+                *head = temp;
+        }
+        free(*head);
+	*head = NULL;
+}
