02833ccfa86c49e7e7b9125aa2b324c6a241f1fd
Ahmed Wael <xahmedwaheed@gmail.com>
2023-09-26 12:19:22 -0700

    eight

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_listint - returns sum
+ * @head: the list
+ *
+ * Description: it does something
+ * Return: 0
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (*head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
