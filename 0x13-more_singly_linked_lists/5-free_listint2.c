e63462ba5b75f32bdf3affae8e0026ab6eff49a6
Ahmed Wael <xahmedwaheed@gmail.com>
2023-09-26 06:42:37 -0700

    five

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,16 +10,16 @@
 
 void free_listint2(listint_t **head)
 {
-        listint_t *temp;
+	listint_t *temp;
 
-        if (*head == NULL)
-                return;
-        while ((*head)->next != NULL)
-        {
-                temp = (*head)->next;
-                free(*head);
-                *head = temp;
-        }
-        free(*head);
+	if (*head == NULL)
+		return;
+	while ((*head)->next != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	free(*head);
 	*head = NULL;
 }
