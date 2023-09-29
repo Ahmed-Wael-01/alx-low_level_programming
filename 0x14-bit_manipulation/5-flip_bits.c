ee2ba192907c8f1f68c01485f7906d0fec320bef
Ahmed Wael <xahmedwaheed@gmail.com>
2023-09-28 19:03:30 -0700

    five

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bits - counts the fliped bits
+ * @n: a number
+ * @m: another number
+ *
+ * Description: it does something
+ * Return: 0
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int fliped = n ^ m;
+	int counter = 0;
+
+	while (fliped)
+	{
+		if (fliped & 1ul)
+			counter++;
+		fliped = fliped >> 1;
+	}
+	return (counter);
+}
