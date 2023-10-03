261f5e1a34f271147fb5510003004393f41d3c80
Ahmed Wael <xahmedwaheed@gmail.com>
2023-10-02 19:35:48 -0700

    one

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * create_file - creates a file
+ * @filename: a var
+ * @text_content: a var
+ *
+ * Description: it does something
+ * Return: 0
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes = 0, len = strlen(text_content);
+
+	if (!filename)
+		return ()-1;
+	fd = open(filename, O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (len)
+		bytes = write(fd, text_content, len);
+	close(fd);
+	return (bytes == len ? 1 : -1);
+}
