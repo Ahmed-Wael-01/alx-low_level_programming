4a4e42467256b7f9d3079a858d95db86a137b312
Ahmed Wael <xahmedwaheed@gmail.com>
2023-10-02 19:36:52 -0700

    one

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	ssize_t bytes = 0, len = strlen(text_content);
 
 	if (!filename)
-		return ()-1;
+		return (-1);
 	fd = open(filename, O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
