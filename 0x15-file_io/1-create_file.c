1f05894c8251bc9fbdd6e2eb2152423b96f3add7
Ahmed Wael <xahmedwaheed@gmail.com>
2023-10-04 06:42:50 -0700

    one

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	dest = open(filename, O_RDONLY | O_CREAT | O_TRUNC, 0600);
+	dest = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (dest == -1)
 		return (-1);
 	if (text_content == NULL)
