076633899674ea36936c0928c1b2d415d540e76d
root <root@DESKTOP-A81GSMJ.localdomain>
2023-10-06 06:09:35 +0300

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,11 +10,44 @@
 
 int main(int argc, char **argv)
 {
-	int from_fd = 0, to_fd = 0;
-	ssize_t b;
-	char buffer[1024];
+	int fd1, fd2, tst;
+	char *buf;
 
 	if (argc != 3)
-		dprintf(STRERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	from_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, );
+	{
+		dprintf(stderr, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
+	{
+		dprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	buf = malloc(1024 * sizeof(char));
+	do {
+		tst = read(fd1, buf, 1024);
+		tst = write(fd2, buf, tst);
+		if (tst == -1)
+		{
+			free(buf);
+			dprintf(stderr, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	} while (tst);
+	free(buf);
+	tst = close(fd1);
+	if (tst == -1)
+	{
+		dprintf(stderr, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	tst = close(fd2);
+	if (tst == -1)
+	{
+		dprintf(stderr, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <string.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
