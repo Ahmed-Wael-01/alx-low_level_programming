ab2214177a71a595a125d5f78c131f41bb3a421f
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2023-11-10 00:34:57 +0200

    zero

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * *print_dlistint - print list
+ * @h: head
+ *
+ * Discription: prints every node in the list
+ * Return: number of nodes*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int counter;
+
+	for (; h != NULL; h = h->next)
+	{
+		printf("%d\n", h->n);
+		counter++;
+	}
+	return (counter);
+}
