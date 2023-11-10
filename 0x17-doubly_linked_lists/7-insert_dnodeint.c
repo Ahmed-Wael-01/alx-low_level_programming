037a38a394cba3aa852b643cc3634554b9a74ea1
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2023-11-10 18:57:42 +0200

    seven

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,6 +32,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			curr->prev = new;
 			if (idx == 0)
 				*h = new;
+			else
+				pre->next = new;
 			break;
 		}
 		pre = curr;
